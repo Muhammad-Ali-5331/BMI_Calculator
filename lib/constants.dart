cbfa00a26f8dbaa03005855bf22513ea7951295e
Muhammad Ali <malitariq5324@gmail.com>
2025-08-21 18:57:06 +0500

    feat: gathered constants from all dart files.

---
+++ lib/constants.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+// # --- Input Page's --- #
+const activeCardColour = Color(0xFF1D1E33);
+const inactiveCarColour = Color(0xFF111328);
+const bottomColor = Color(0xFFEB1555);
+const iconsSize = 60.0;
+const cardWidth = 200.0;
+const cardHeight = 170.0;
+const numbersTextStyle = TextStyle(
+  color: Colors.white,
+  fontSize: 50.0,
+  fontWeight: FontWeight.w900,
+);
+
+// # --- Icons Page's --- #
+const sizeBoxHeight = 15.0;
+const textColor = Color(0xFF8D8E98);
+const fontSize = 18.0;
+
+// # --- Main Page's --- #
+const smallTextStyle = TextStyle(color: textColor, fontSize: 23.5);
+
+// # --- Card's --- #
+const borderRadius = 15.0;
+const widgetsMargin = 15.0;
