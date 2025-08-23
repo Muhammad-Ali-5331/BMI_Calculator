5ab6cd8b92406ac8420b798752c12920e6ffc000
Muhammad Ali <malitariq5324@gmail.com>
2025-08-23 19:28:45 +0500

    Added ResultsPage screen with basic layout
    Created a new ResultsPage widget that has its own Scaffold and AppBar.
    Right now, it only shows a simple “Hi, Welcome!” text in the body, but this
    sets up the structure for the BMI results screen that I’ll expand later.

---
+++ lib/results_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ResultsPage extends StatelessWidget {
+  const ResultsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(centerTitle: true, title: Text('BMI CALCULATOR')),
+      body: Text('Hi, Welcome!'),
+    );
+  }
+}
